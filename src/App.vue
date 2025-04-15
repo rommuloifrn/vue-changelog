8b7a7b7bb552d4d26afb1454dba9de097260e9ba
rommuloifrn <roomulo.cavalkant@outlook.com>
2025-04-15 13:30:48 -0300

    feat: ajustes no header da página

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-  import { RouterLink, RouterView } from 'vue-router'
-  
-
-
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  
-  <router-link to="/" class="text-4xl font-medium">
-    Changelog
-    </router-link>
-  <RouterView/>
+  <router-link to="/">
+    <div class="mb-5 mt-20">
+      <span class="text-6xl font-bold">HUB SEBRAE RN <br> Changelog </span>
 
+      <p class="text-base font-normal mt-4">
+        A <i>sua</i> fonte de informação sobre a <i>nossa</i> entrega.
+      </p>
+    </div>
+  </router-link>
+
+  <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
